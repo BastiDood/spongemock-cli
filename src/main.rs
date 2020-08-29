7d236c6c1db671b2e06de4c81d76ccc3b4025156
Basti Ortiz <ortizbasti16@gmail.com>
2020-08-29 18:47:31 +0800

    Feature: echo input string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+use rand;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args().skip(1);
+    let text = args.next().unwrap_or("--help".to_owned());
+
+    if text == "--help" {
+        println!("spongemock [--help] <text>");
+        return;
+    }
+
+    println!("{}", &text);
 }
