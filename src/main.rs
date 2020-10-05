41d89c41e2cb39d3ddba1eb02e37468a79ae414c
Basti Ortiz <ortizbasti16@gmail.com>
2020-10-05 19:29:02 +0800

    Feature: enable input from `stdin`

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use std::env;
+use std::{ env, io::{ self, Read } };
 use rand::random;
 
 fn main() {
     let mut args = env::args().skip(1);
-    let text = args.next().unwrap_or("--help".to_owned());
+    let input = args.next();
 
-    if text == "--help" {
-        println!("spongemock [--help] <text>");
-        return;
-    }
+    let text = match input {
+        Some(text) => {
+            if let "--help" | "-h" | "" = text.as_str() {
+                println!("spongemock [--help, -h] [text]");
+                return;
+            }
+            text
+        },
+        None => {
+            let mut buffer = String::with_capacity(32);
+            io::stdin().lock().read_to_string(&mut buffer).unwrap();
+            buffer
+        },
+    };
 
     let result: String = text
         .into_bytes()
@@ -21,5 +31,6 @@ fn main() {
         })
         .collect();
 
-    println!("{}", result.as_str());
+    print!("{}", result.trim());
+
 }
