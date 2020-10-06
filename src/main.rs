1ce2e5e91d951e230f4f404d59d9e4e67161bfb0
Basti Ortiz <ortizbasti16@gmail.com>
2020-10-06 12:10:53 +0800

    Perf: directly manipulate the bytes of the underlying vector of bytes

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let mut args = env::args().skip(1);
     let input = args.next();
 
-    let text = match input {
+    let mut text = match input {
         Some(text) => {
             if let "--help" | "-h"  = text.as_str() {
                 println!("spongemock [--help, -h] [text]");
@@ -20,17 +20,15 @@ fn main() {
         },
     };
 
-    let result: String = text
-        .into_bytes()
-        .into_iter()
-        .map(|byte| {
-            let mut ch: char = byte.into();
-            if random() { ch.make_ascii_uppercase(); }
-            else { ch.make_ascii_lowercase(); }
-            ch
-        })
-        .collect();
+    // SAFETY: Since we are looking into a mutable slice
+    // into an already valid `String`, the bytes have already
+    // been checked beforehand for UTF-8 compliance.
+    let underlying_vector = unsafe { text.as_mut_vec() };
+    for byte in underlying_vector.iter_mut() {
+        if random() { byte.make_ascii_uppercase(); }
+        else { byte.make_ascii_lowercase(); }
+    } 
 
-    print!("{}", result.trim());
+    print!("{}", &text);
 
 }
