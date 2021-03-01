2752cf55ba7bed319c44b8681074195e323b18f7
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-03-01 20:35:59 +0800

    Refactor: use more idiomatic syntax

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
-use std::{ env, io::{ self, Read } };
 use rand::random;
+use std::{
+    env,
+    io::{stdin, Read},
+};
 
 fn main() {
     let mut args = env::args().skip(1);
-    let input = args.next();
+    let input = args.next().unwrap_or_else(|| {
+        let mut buffer = String::with_capacity(32);
+        stdin()
+            .lock()
+            .read_to_string(&mut buffer)
+            .expect("unable to read input");
+        buffer
+    });
 
-    let text = match input {
-        Some(text) => {
-            if let "--help" | "-h"  = text.as_str() {
-                println!("spongemock [--help, -h] [text]");
-                return;
-            }
-            text
-        },
-        None => {
-            let mut buffer = String::with_capacity(32);
-            io::stdin().lock().read_to_string(&mut buffer).unwrap();
-            buffer
-        },
-    };
+    if let "--help" | "-h" = input.as_str() {
+        println!("spongemock [--help, -h] [text]");
+        return;
+    }
 
-    let spongemocked: String = text
+    let spongemocked: String = input
         .trim()
         .chars()
-        .map(|c| if random() { c.to_ascii_uppercase() } else { c.to_ascii_lowercase() })
+        .map(|c| {
+            if random() {
+                c.to_ascii_uppercase()
+            } else {
+                c.to_ascii_lowercase()
+            }
+        })
         .collect();
 
-    print!("{}", spongemocked.as_str());
+    print!("{}", spongemocked);
 }
