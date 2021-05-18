0bb21724122c23d9d2e5d75612896604f14b215f
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-05-18 14:59:14 +0800

    Refactor: use Clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use rand::random;
 use std::{
     env,
@@ -5,21 +6,44 @@ use std::{
 };
 
 fn main() {
-    let mut args = env::args().skip(1);
-    let input = args.next().unwrap_or_else(|| {
-        let mut buffer = String::with_capacity(32);
-        stdin()
-            .lock()
-            .read_to_string(&mut buffer)
-            .expect("unable to read input");
-        buffer
-    });
+    // Set up CLI
+    let matches = App::new("Spongemock CLI")
+        .bin_name("spongemock")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(Arg::with_name("input"))
+        .get_matches();
 
-    if let "--help" | "-h" = input.as_str() {
-        println!("spongemock [--help, -h] [text]");
+    // Parse results
+    let mut input = matches
+        .value_of("input")
+        .map(ToOwned::to_owned)
+        .unwrap_or_else(|| {
+            let mut buffer = String::with_capacity(32);
+            stdin()
+                .lock()
+                .read_to_string(&mut buffer)
+                .expect("unable to read from standard input");
+            buffer
+        });
+
+    // Optimize the case when all characters are ASCII-based
+    if input.is_ascii() {
+        // SAFETY: The input has been verified to be within the ASCII range.
+        let ascii_bytes = unsafe { input.as_bytes_mut() };
+        for byte in ascii_bytes {
+            if random() {
+                byte.make_ascii_uppercase();
+            } else {
+                byte.make_ascii_lowercase();
+            }
+        }
+        print!("{}", input);
         return;
     }
 
+    // Otherwise, implement the less efficient version which requires copying
     let spongemocked: String = input
         .trim()
         .chars()
@@ -32,6 +56,5 @@ fn main() {
             c
         })
         .collect();
-
     print!("{}", spongemocked);
 }
