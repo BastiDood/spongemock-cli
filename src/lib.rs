5837961757ab32fb0efb6de4b7f8c24cf78a2957
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2022-02-05 20:56:35 +0800

    Test: add tests for the logic

--- src/lib.rs
+++ src/lib.rs
@@ -46,7 +46,7 @@ where
         let text = unsafe { core::str::from_utf8(&char_be[..leading_ones]).unwrap_unchecked() };
         for ch in text.chars() {
             let mut buf = [0; 4];
-            if rng.gen_bool(0.5) {
+            if rng.gen() {
                 for c in ch.to_uppercase() {
                     let bytes = c.encode_utf8(&mut buf).as_bytes();
                     dest.write_all(bytes)?;
@@ -61,4 +61,47 @@ where
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::spongemock;
+    use rand::rngs::mock::StepRng;
+
+    /// Since the [`StepRng`](StepRng) mock RNG samples Booleans by
+    /// using the most significant bit of the `u32` returned from the
+    /// [`RngCore::next_u32`](rand::RngCore::next_u32) method, we set
+    /// the increment as defined below.
+    ///
+    /// This specific integer allows us to toggle the most
+    /// significant bit after every incrementation. We simply
+    /// exploit the fact that `StepRng` performs wrapping addition.
+    const INCREMENT: u64 = 0x_0000_0000_8000_0000;
+
+    #[test]
+    fn ascii_only() {
+        let bytes = "Hello".bytes().map(Ok);
+        let mut dest = Vec::new();
+        let mut rng = StepRng::new(0, INCREMENT);
+        spongemock(bytes, &mut dest, &mut rng).unwrap();
+        assert_eq!(&dest, "hElLo".as_bytes());
+    }
+
+    #[test]
+    fn with_greek_characters() {
+        let bytes = "Ηελλο".bytes().map(Ok);
+        let mut dest = Vec::new();
+        let mut rng = StepRng::new(0, INCREMENT);
+        spongemock(bytes, &mut dest, &mut rng).unwrap();
+        assert_eq!(&dest, "ηΕλΛο".as_bytes());
+    }
+
+    #[test]
+    fn with_mixed_greek_and_ascii() {
+        let bytes = "Ηελλο World".bytes().map(Ok);
+        let mut dest = Vec::new();
+        let mut rng = StepRng::new(0, INCREMENT);
+        spongemock(bytes, &mut dest, &mut rng).unwrap();
+        assert_eq!(&dest, "ηΕλΛο wOrLd".as_bytes());
+    }
 }
