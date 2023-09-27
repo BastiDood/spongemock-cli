581ed036cc86a2b95593fb2932591ba3440dfe4e
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-09-28 02:30:41 +0800

    refactor: simplify implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use rand::{rngs::SmallRng, SeedableRng};
-use spongemock::spongemock;
-use std::io::{stdin, stdout, Read, Result};
+use std::io::{stdin, stdout, Result, Write as _};
+use utf8_chars::BufReadCharsExt as _;
 
 fn main() -> Result<()> {
-    let mut dest = stdout();
-    let mut rng = SmallRng::from_entropy();
-    spongemock(stdin().bytes(), &mut dest, &mut rng)
+    let mut rng = nanorand::WyRand::new();
+    let mut stdout = stdout().lock();
+    for c in stdin().lock().chars() {
+        let c = c?;
+        if nanorand::RandomGen::random(&mut rng) {
+            for c in c.to_uppercase() {
+                write!(&mut stdout, "{c}")?;
+            }
+        } else {
+            for c in c.to_lowercase() {
+                write!(&mut stdout, "{c}")?;
+            }
+        };
+    }
+    Ok(())
 }
